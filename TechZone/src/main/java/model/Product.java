0a3e985c110ace82074b36ea4a7d4e1b3c8179d1
Hoang Han <hannh.ce192048@gmail.com>
2025-09-20 12:45:10 +0700

    sua lai cac field trong controller

--- TechZone/src/main/java/model/Product.java
+++ TechZone/src/main/java/model/Product.java
@@ -4,6 +4,10 @@
  */
 package model;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
 /**
  *
  * @author letan
@@ -12,24 +16,34 @@ public class Product {
     private int productId;
     private String linkImg;
     private String productName;
-    private String productPrice;
-    private String productIdDetail;
-    private String categoryId;
-    private String isDeleted;
-    private String timeCreate;
+    private BigDecimal productPrice;
+    private Map<String, String> productAttributes;
+    private int categoryId;
+    private boolean isDeleted;
+    private LocalDateTime timeCreate;
+    private int quantity;
 
     public Product() {
     }
 
-    public Product(int productId, String linkImg, String productName, String productPrice, String productIdDetail, String categotyId, String isDeleted, String timeCreate) {
+    public Product(int productId, String linkImg, String productName, BigDecimal productPrice, Map<String, String> productAttributes, int categoryId, boolean isDeleted, LocalDateTime timeCreate, int quantity) {
         this.productId = productId;
         this.linkImg = linkImg;
         this.productName = productName;
         this.productPrice = productPrice;
-        this.productIdDetail = productIdDetail;
-        this.categoryId = categotyId;
+        this.productAttributes = productAttributes;
+        this.categoryId = categoryId;
         this.isDeleted = isDeleted;
         this.timeCreate = timeCreate;
+        this.quantity = quantity;
+    }
+
+    public Map<String, String> getProductAttributes() {
+        return productAttributes;
+    }
+
+    public void setProductAttributes(Map<String, String> productAttributes) {
+        this.productAttributes = productAttributes;
     }
 
     public int getProductId() {
@@ -56,44 +70,46 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getProductPrice() {
+    public BigDecimal getProductPrice() {
         return productPrice;
     }
 
-    public void setProductPrice(String productPrice) {
+    public void setProductPrice(BigDecimal productPrice) {
         this.productPrice = productPrice;
     }
 
-    public String getProductIdDetail() {
-        return productIdDetail;
-    }
 
-    public void setProductIdDetail(String productIdDetail) {
-        this.productIdDetail = productIdDetail;
-    }
-
-    public String getCategotyId() {
+    public int getCategoryId() {
         return categoryId;
     }
 
-    public void setCategotyId(String categotyId) {
-        this.categoryId = categotyId;
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 
-    public String getIsDeleted() {
+    public boolean isIsDeleted() {
         return isDeleted;
     }
 
-    public void setIsDeleted(String isDeleted) {
+    public void setIsDeleted(boolean isDeleted) {
         this.isDeleted = isDeleted;
     }
 
-    public String getTimeCreate() {
+    public LocalDateTime getTimeCreate() {
         return timeCreate;
     }
 
-    public void setTimeCreate(String timeCreate) {
+    public void setTimeCreate(LocalDateTime timeCreate) {
         this.timeCreate = timeCreate;
     }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     
 }
---
+++ TechZone/src/main/webapp/WEB-INF/views/user/cart.jsp
@@ -0,0 +1,28 @@
+<%-- 
+    Document   : cart
+    Created on : Sep 15, 2025, 4:26:22 PM
+    Author     : NgKaitou
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1> project prj</h1>
+        <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+        <script src=""  ></script>
+    </body>
+
+</html>
