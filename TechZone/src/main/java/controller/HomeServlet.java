750e482f5a6d8390ced1574a0215a7d6d690cf6c
Hoang Han <hannh.ce192048@gmail.com>
2025-09-22 08:03:55 +0700

    navbar

--- TechZone/src/main/java/controller/HomeServlet.java
+++ TechZone/src/main/java/controller/HomeServlet.java
@@ -45,7 +45,7 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/Views/Pages/Home.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/views/Includes/Navbar.jsp").forward(request, response);
     }
 
     /**
