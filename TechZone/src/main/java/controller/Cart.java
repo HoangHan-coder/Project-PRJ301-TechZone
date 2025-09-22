07589db0d2239acc85dd5ca049fb02c431de49bb
yuohta819 <thunhan798@gmail.com>
2025-09-22 07:52:42 +0700

    merge

--- TechZone/src/main/java/controller/Cart.java
+++ TechZone/src/main/java/controller/Cart.java
@@ -57,7 +57,7 @@ public class Cart extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("/WEB-INF/Views/user/cart.jsp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/views/user/cart.jsp").forward(request, response);
     }
 
     /**
