071506c5772d435fb9d12961189eefa49c27f42f
Hoang Han <hannh.ce192048@gmail.com>
2025-10-24 19:58:51 +0700

    fix lỗi DBContext

--- TechZone/src/main/java/dao/ProductDAO.java
+++ TechZone/src/main/java/dao/ProductDAO.java
@@ -1,19 +1,18 @@
 package dao;
 
-import db.DBUntils;
+import db.DBContext;
 import java.sql.*;
 import java.util.*;
-import java.math.BigDecimal;
 import model.Product;
 
-public class ProductDAO extends DBUntils {
+public class ProductDAO extends DBContext {
 
     // ✅ Lấy tất cả sản phẩm (dành cho user)
     public List<Product> getAllProducts() {
         List<Product> list = new ArrayList<>();
         String sql = "SELECT * FROM Product WHERE IsDeleted = 0";
 
-        try (Connection con = DBUntils.getConnection(); PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+        try (Connection con = this.getConnection(); PreparedStatement ps = con.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
                 list.add(mapResultSetToProduct(rs));
@@ -30,7 +29,7 @@ public class ProductDAO extends DBUntils {
     public Product getProductById(int id) {
         String sql = "SELECT * FROM Product WHERE ProductId = ? AND IsDeleted = 0";
 
-        try (Connection con = DBUntils.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+        try (Connection con = this.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
 
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
@@ -51,7 +50,7 @@ public class ProductDAO extends DBUntils {
         List<Product> list = new ArrayList<>();
         String sql = "SELECT * FROM Product WHERE CategoryId = ? AND IsDeleted = 0";
 
-        try (Connection con = DBUntils.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+        try (Connection con = this.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
 
             ps.setInt(1, categoryId);
             ResultSet rs = ps.executeQuery();
@@ -92,7 +91,7 @@ public class ProductDAO extends DBUntils {
         List<Product> list = new ArrayList<>();
         String sql = "SELECT TOP 1 * FROM Product WHERE categoryId = ? ORDER BY createdAt DESC";
 
-        try (Connection con = DBUntils.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+        try (Connection con = this.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
 
             ps.setInt(1, categoryId);
             ResultSet rs = ps.executeQuery();
@@ -110,7 +109,7 @@ public class ProductDAO extends DBUntils {
         List<Product> list = new ArrayList<>();
         String sql = "SELECT TOP 1 * FROM Product WHERE CategoryId = ? ORDER BY quantitySold DESC";
 
-        try (Connection con = DBUntils.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+        try (Connection con = this.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
 
             ps.setInt(1, categoryId);
             ResultSet rs = ps.executeQuery();
@@ -131,7 +130,7 @@ public class ProductDAO extends DBUntils {
         sql += " AND JSON_VALUE(ProductAttributes, '$.brand') = ?";
     }
 
-    try (Connection con = DBUntils.getConnection();
+    try (Connection con = this.getConnection();
          PreparedStatement ps = con.prepareStatement(sql)) {
 
         ps.setInt(1, categoryId);
