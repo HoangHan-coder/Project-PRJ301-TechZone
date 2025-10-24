73f8cff4f29ef40480fee0d28e83af094561d7d5
Hoang Han <hannh.ce192048@gmail.com>
2025-10-24 22:20:30 +0700

    translate foreach

--- TechZone/src/main/java/until/Normalization.java
+++ TechZone/src/main/java/until/Normalization.java
@@ -17,9 +17,9 @@ public class Normalization {
         StringBuilder normalizedName = new StringBuilder();
         
         String parts[] = name.trim().split("\\s+");
-        for (int i = 0; i < parts.length; i++) {
-            char firstLetter = Character.toUpperCase(parts[i].trim().charAt(0));
-            String tail = parts[i].trim().substring(1).toLowerCase();
+        for (String part : parts) {
+            char firstLetter = Character.toUpperCase(part.trim().charAt(0));
+            String tail = part.trim().substring(1).toLowerCase();
             normalizedName.append(firstLetter).append(tail).append(" ");
         }
         return normalizedName.toString().trim();
