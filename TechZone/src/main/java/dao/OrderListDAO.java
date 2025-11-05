be9ab9af0a5673d7338937991d169d03f04b13a8
yuohta819 <thunhan798@gmail.com>
2025-11-05 09:40:25 +0700

    update

--- TechZone/src/main/java/dao/OrderListDAO.java
+++ TechZone/src/main/java/dao/OrderListDAO.java
@@ -214,7 +214,7 @@ public class OrderListDAO extends DBContext {
 
     public ResponseOrder getResponse(int id) {
         try {
-            String sql = "SELECT * FROM responseOrder WHERE responseOrderId = ?";
+            String sql = "SELECT * FROM responseOrder WHERE orderId = ?";
             PreparedStatement st = this.getConnection().prepareStatement(sql);
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
